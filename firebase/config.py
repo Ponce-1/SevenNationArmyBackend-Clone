c34d01213150952f124537e660d83b8b9bf63c0c
Tomas <37884033+Ponce-1@users.noreply.github.com>
2020-03-05 01:20:22 -0800

    Add files via upload

---
+++ firebase/config.py
@@ -0,0 +1,11 @@
+
+
+config = {
+    "apiKey": "AIzaSyD7723JgXG7S-zZ4xaERl26ysbY0AAoL5w",
+    "authDomain": "cecs475-b8e5c.firebaseapp.com",
+    "databaseURL": "https://cecs475-b8e5c.firebaseio.com",
+    "projectID": "cecs475-b8e5c",
+    "storageBucket": "cecs475-b8e5c.appspot.com",
+    "messagingSenderId": "950704171794"
+}
+
